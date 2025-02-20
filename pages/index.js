20cb245359e65b4c826d0db57979855af17cb97a
Anderson Martins <andrews.cs16@gmail.com>
2025-02-20 11:31:16 -0300

    Atualizando

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Ana Teresa gata demais!!!</h1>
+    return (
+        <div>
+            <h1>Ana Teresa gata demais!!!</h1>
+            <img alt="mulher gata de cabelos pretos fingindo ser uma DJ" src="https://lh3.googleusercontent.com/pw/AP1GczMODhGT20Ce2wXij1aM6B0f_WZrbvm_TDy7u-myHAbuMMz1uNRagWPMbkPIWLAX1U-5IlFJDPhbvDB6DA7MhhzxjbqezJlE7U8Odtz0qdXATwIuABx_jCIQqdU_jATwfxmuGUA2kGl_wEPdXe7BwqCjIg=w480-h853-s-no-gm?authuser=0" />
+        </div>
+    );
 }
 
 export default Home;

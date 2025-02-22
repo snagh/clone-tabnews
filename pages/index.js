17995747261f4ff823a8ef9dcf316fe70aba4fff
Anderson Martins <andrews.cs16@gmail.com>
2025-02-22 18:42:12 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 function Home() {
-    return (
-        <div>
-            <h1>Ana Teresa gata demais!!!</h1>
-            <img alt="mulher gata de cabelos pretos fingindo ser uma DJ" src="https://lh3.googleusercontent.com/pw/AP1GczMODhGT20Ce2wXij1aM6B0f_WZrbvm_TDy7u-myHAbuMMz1uNRagWPMbkPIWLAX1U-5IlFJDPhbvDB6DA7MhhzxjbqezJlE7U8Odtz0qdXATwIuABx_jCIQqdU_jATwfxmuGUA2kGl_wEPdXe7BwqCjIg=w480-h853-s-no-gm?authuser=0" />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Ana Teresa gata demais!!!</h1>
+      <img
+        alt="mulher gata de cabelos pretos fingindo ser uma DJ"
+        src="https://lh3.googleusercontent.com/pw/AP1GczMODhGT20Ce2wXij1aM6B0f_WZrbvm_TDy7u-myHAbuMMz1uNRagWPMbkPIWLAX1U-5IlFJDPhbvDB6DA7MhhzxjbqezJlE7U8Odtz0qdXATwIuABx_jCIQqdU_jATwfxmuGUA2kGl_wEPdXe7BwqCjIg=w480-h853-s-no-gm?authuser=0"
+      />
+    </div>
+  );
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
-
